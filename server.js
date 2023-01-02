7d6386c179edf7914aed8a4401cab3944f80d834
suraj <jsuraj61@gmail.com>
2023-01-02 19:28:06 +0530

    new

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app=express();
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html')
 })
+app.get('/index2.html',function(req,res){
+    res.sendFile(__dirname + '/index2.html')
+})
 
 app.listen(3000);
 console.log("Connected")
